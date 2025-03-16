b8951e9b0dcc2b327c2b232b057e4dfce578324a
yahav3801 <ybh00790@gmail.com>
2025-03-16 21:56:48 +0200

    mongo setup not much

--- config/env.js
+++ config/env.js
@@ -2,4 +2,4 @@ import { config } from "dotenv";
 
 config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
 
-export const { PORT, NODE_ENV } = process.env;
+export const { PORT, NODE_ENV, DATABASE_URL } = process.env;
---
+++ database/mongodb.js
@@ -0,0 +1,2 @@
+import mongoose from "mongoose";
+import { DATABASE_URL, NODE_ENV } from "../config/env";
